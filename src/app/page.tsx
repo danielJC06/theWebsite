8de43a1c350dcdc9471cf626e5ecc590788921bd
danieljc06 <119332996+danielSoftDev@users.noreply.github.com>
2023-09-18 16:11:42 -0500

    apos fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent 
       before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
 			>
-				<div>I Hope You're Hungry:</div>
+				<div>I Hope You`&apos;`re Hungry:</div>
 				<div>{`We're Grilling ${tastyFood || "Hot Dogs"}`}</div>
 			</div>
 		</main>
