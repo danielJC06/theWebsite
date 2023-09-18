1f7d45784842218a3d93d09b08912308d67c02fa
danieljc06 <119332996+danielSoftDev@users.noreply.github.com>
2023-09-18 16:24:42 -0500

    home button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from 'next/dist/server/api-utils';
 import Image from 'next/image';
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
 			>
 				<div>I Hope You&apos;re Hungry:</div>
 				<div>{`We're Grilling ${tastyFood || "Hot Dogs"}`}</div>
+				<button onClick={()=>window.open("https://youtu.be/dQw4w9WgXcQ", "_blank")}>Home</button>
 			</div>
 		</main>
 	);
