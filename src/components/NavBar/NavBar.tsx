7733b2c31fe70e548e2ef6729f59640c3041f76a
Daniel Cox <danieljeancox@gmail.com>
2023-09-21 11:25:49 -0500

    bootstrap added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,22 @@
+import Dropdown from "react-bootstrap/Dropdown";
 
 function NavBar(props: any) {
   return (
     <>
-    <div>NavBar</div>
-    {props.children}
-    </>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
 
-  )
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      {props.children}
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
